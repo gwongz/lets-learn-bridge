90796aafbb8898f972b54e554d8b9a8a33620a37
gwongz <gwongz@gmail.com>
2014-10-26 11:29:27 -0700

    adding comment

--- server/publications.js
+++ server/publications.js
@@ -3,7 +3,7 @@ Meteor.publish('questions', function() {
 });
 
 Meteor.publish('userData', function () {
-	// only publish all userData if user is superuser
+	// only publish data for all users if logged in user is superuser
 	if (isSuperuser(this.userId)){
 		return Meteor.users.find({}, {fields: {'_id':1, 'username': 1}});
 	}
