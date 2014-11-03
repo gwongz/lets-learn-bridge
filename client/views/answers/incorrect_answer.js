c5cf0be92e773dcdf78189bd33123bbf9a8db400
gwongz <gwongz@gmail.com>
2014-11-03 13:01:10 -0800

    removing confetti effect

--- client/views/answers/incorrect_answer.js
+++ client/views/answers/incorrect_answer.js
@@ -1,11 +1,10 @@
 Template.incorrectAnswer.events({
   'click .hint': function(){
-
     Meteor.call('getAnswer', this._id, function(err, response){
       Session.set('answer', response);
     });
 
-  $('.revealed-answer').removeClass('hidden');
+    $('.revealed-answer').removeClass('hidden');
 
   }
 });
